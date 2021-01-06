1050f0abdaa68a06ae574acfed8fd8d348c6056a
tanimutomo <tanimutomo@gmail.com>
2021-01-07 00:09:59 +0900

    add: project_path to flags

--- modelgen/modelgen.go
+++ modelgen/modelgen.go
@@ -6,18 +6,19 @@ import (
 
 	"github.com/iancoleman/strcase"
 	"github.com/ispec-inc/civgen-go/modelgen/model"
+	"github.com/ispec-inc/civgen-go/modelgen/pkg"
 )
 
 var (
 	// Required
-	name   = flag.String("name", "", "Model name")
-	fields = flag.String("fields", "", "Fields of the model (e.g. ID:string,Name:string,CreatedAt:time.Time,Update:time.Time")
+	name        = flag.String("name", "", "Model name")
+	fields      = flag.String("fields", "", "Fields of the model (e.g. ID:string,Name:string,CreatedAt:time.Time,Update:time.Time")
+	projectPath = flag.String("project_path", "", "Go package path of this project root")
 
 	// Optional
-	baseDir    = flag.String("base_dir", ".", "Path to base directory")
-	entityPath = flag.String("entity_path", "pkg/infra/entity", "Path to entity directory")
-	modelPath  = flag.String("model_path", "pkg/domain/model", "Path to model directory")
-	viewPath   = flag.String("view_path", "pkg/view", "Path to view directory")
+	entityPath = flag.String("entity_path", "pkg/infra/entity", "Relative path to the entity directory from 'project_root'")
+	modelPath  = flag.String("model_path", "pkg/domain/model", "Relative path to the model directory from 'project_root'")
+	viewPath   = flag.String("view_path", "pkg/view", "Relative path to the view directory from 'project_root'")
 
 	createEntity = flag.Bool("create_entity", true, "Create entity file, if true")
 	createModel  = flag.Bool("create_model", true, "Create model file, if true")
@@ -26,6 +27,12 @@ var (
 
 func main() {
 	flag.Parse()
+
+	if err := setPackages(); err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
 	generateModelFile(model.LayerEntity)
 	generateModelFile(model.LayerModel)
 	generateModelFile(model.LayerView)
@@ -52,7 +59,7 @@ func generateModelFile(layer model.Layer) {
 		path = *viewPath
 	}
 
-	filepath := fmt.Sprintf("%s/%s/%s.go", *baseDir, path, strcase.ToSnake(*name))
+	filepath := fmt.Sprintf("%s/%s.go", path, strcase.ToSnake(*name))
 
 	if !doCreate {
 		return
@@ -72,4 +79,14 @@ func generateModelFile(layer model.Layer) {
 	}
 
 	fmt.Printf("Generate %s file successfully to '%s'\n", layer.String(), filepath)
+}
+
+func setPackages() error {
+	ipt := pkg.SetPkgsInput{
+		ProjectRoot: *projectPath,
+		EntityPath:  *entityPath,
+		ModelPath:   *modelPath,
+		ViewPath:    *viewPath,
+	}
+	return pkg.SetPkgs(ipt)
 }
